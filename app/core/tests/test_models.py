2e815dfb2c70ff868050b7d080beb08927cbfe40
skabaza <skabaza46@gmail.com>
2022-11-03 13:35:49 -0400

    added superuser creation and test case for creating superuser.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -43,4 +43,13 @@ class ModelTests(TestCase):
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user("", "test12344")
 
-            
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+        user = get_user_model().objects.create_superuser(
+            "test@example.com",
+            "test1234"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+        
