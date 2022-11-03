cf36196acedc9fd37f097ab6db3036226b1b5195
skabaza <skabaza46@gmail.com>
2022-11-03 17:07:33 -0400

    Implemented token API.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -63,9 +63,10 @@ class PublicUserAPITests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-        user_exists = get_user_model().objects.get(
+        user_exists = get_user_model().objects.filter(
             email=payload['email']
         ).exists()
+
         self.assertFalse(user_exists)
 
     def test_create_token_for_user(self):
@@ -105,4 +106,4 @@ class PublicUserAPITests(TestCase):
         res =self.client.post(TOKEN_URL, payload)
 
         self.assertNotIn("token", res.data)
-        self.assertEqual(rest.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
