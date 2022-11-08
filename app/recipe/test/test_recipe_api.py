d1c416f4df57a92cba3918cb27e4b70f8f335dc0
skabaza <skabaza46@gmail.com>
2022-11-08 08:39:37 -0500

    Implemented recipe filter feature.

--- app/recipe/test/test_recipe_api.py
+++ app/recipe/test/test_recipe_api.py
@@ -431,7 +431,7 @@ class PrivateRecipeAPITests(TestCase):
 
         self.assertIn(s1.data, res.data)
         self.assertIn(s2.data, res.data)
-        self.assertNotIn(s3.data, rest.data)
+        self.assertNotIn(s3.data, res.data)
 
     def test_filter_by_ingredients(self):
         """Test filting recipe by ingredients."""
