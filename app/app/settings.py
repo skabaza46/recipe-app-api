d6e04a622adc7f61193f4eefebbe5b7cc626f84a
skabaza <skabaza46@gmail.com>
2023-01-19 10:51:05 -0500

    formatted code with black.

--- app/app/settings.py
+++ app/app/settings.py
@@ -151,7 +151,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
 }
 
 # Allows the image to be uploaded from the browser interface
